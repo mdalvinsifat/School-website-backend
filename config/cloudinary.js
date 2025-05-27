12a3d6c2faf809d48ffc1b0d6950cb6fbd743ebe
mdalvinsifat <mdalvinsifat@gmail.com>
2025-05-27 16:58:48 +0600

    last

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,9 +1,10 @@
+// cloudinaryConfig.js
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name:"ddgfykwjt",
-  api_key:"984885397136126",
-  api_secret:"yUll6a1wKVSBjQVZNRpQpYbqaFk"
+  cloud_name: "ddgfykwjt",
+  api_key: "984885397136126",
+  api_secret: "yUll6a1wKVSBjQVZNRpQpYbqaFk"
 });
 
 module.exports = cloudinary;
--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -1,15 +1,17 @@
+// uploadMiddleware.js
 const multer = require("multer");
 const path = require("path");
-const upload= multer({
-  storage: multer.diskStorage({}),
+
+const upload = multer({
+  storage: multer.diskStorage({}), // You may want to add destination here
   fileFilter: (req, file, cb) => {
-    let ext = path.extname(file.originalname);  
+    const ext = path.extname(file.originalname).toLowerCase();
     if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
       cb(new Error("File type is not supported"), false);
       return;
     }
     cb(null, true);
-  },
+  }
 });
 
-module.exports = upload
+module.exports = upload;
